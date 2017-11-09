e19fe2a5fcd7a78a9f74ab65ea289c5ba340a048
stoopfrench <32528005+stoopfrench@users.noreply.github.com>
2017-11-09 00:10:41 -0700

    initial commit

---
+++ public/js/main.js
@@ -0,0 +1,88 @@
+var mainVm = new Vue({
+
+	el: '#app',
+	data: {
+
+		tasks: [],
+		newTasks: 
+		{
+			taskName: '',
+			isCompleted: false,
+		},
+		
+
+},
+	methods: {
+
+		postNewTask: function(event){
+
+			event.preventDefault()
+
+			var task = mainVm.newTasks
+			
+			// console.log(task)
+
+			if(task.taskName === "") {
+				
+				console.log('empty input')
+			}
+
+			else {
+		
+			$.post('/todo', task, function(data){
+
+				// console.log(data)
+				
+				mainVm.getNewTasks()
+
+				mainVm.newTasks.taskName = ""		
+
+
+			})
+
+		}
+
+		
+	},
+
+		getNewTasks: function(){
+
+			$.get('/todo', function(data){
+
+				mainVm.tasks = data
+				
+				// console.log(data)
+
+			})
+		},
+
+		completeTask: function(task){
+
+			task.isCompleted = !task.isCompleted
+
+			console.log(task.isCompleted)
+
+		},
+
+
+		removeTask: function(task){
+
+
+	      	var index = this.tasks.indexOf(task);
+
+	      	this.tasks.splice(index, 1);
+
+	      	$.post('/delete', {taskName:task.taskName}, function(data){
+
+	      		console.log('deleted: ' + task.taskName)
+
+	      	})
+    
+		},
+
+},
+	created: function(){
+
+		this.getNewTasks()
+	}
+})
---
+++ server.js
@@ -0,0 +1,93 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var mongo = require('mongodb')
+var ObjectId = require('mongodb').ObjectID
+
+var app = express()
+
+var MongoClient = mongo.MongoClient
+
+MongoClient.connect('mongodb://localHost:27017/mongo-todo', function(err, db) {
+
+	if(err) {
+
+		console.log('failed to connect to mongo')
+		console.log(err)
+	}
+
+		console.log('success')
+
+	app.use(express.static('./public'))
+
+	app.use(bodyParser.urlencoded({extended: true}))
+	app.use(bodyParser.json())
+
+
+	app.get('/', function(req, res) {
+
+		res.sendFile('./html/index.html', {root: './public'})
+	})
+
+	app.post('/todo', function(req, res){
+
+		body = req.body
+		
+		// console.log(body)	
+
+			db.collection('Tasks').insert(body, function(err){
+
+				if(err){
+					
+					console.log(err)
+					
+					res.send('something went wrong')
+				}
+				
+			})
+
+			res.send('recieved')
+
+	})
+
+	app.get('/todo', function(req, res){
+
+		db.collection('Tasks').find().toArray(function(err, docs){
+
+			if(err) {
+
+				console.log(err)
+				
+				res.send('something went wrong')
+			}
+
+			res.send(docs)
+		})
+	})
+
+	app.post('/delete', function(req, res){
+
+		body = req.body
+
+		// console.log(body)
+
+		db.collection('Tasks').remove({taskName: body.taskName}, function(err, results){
+			
+			if(err){
+
+				console.log(err)
+			}
+
+			// console.log(results)
+
+			res.send('item deleted')
+		})	
+	})
+
+
+
+	app.listen(8082, function(){
+
+		console.log("'mongo-todo' running on port 8082")
+	})
+})
+
